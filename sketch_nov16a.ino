615ebb166c630550e53122d91fe9ab8030b01bb7
Matt <praterma2@hotmail.com>
2021-11-16 15:41:28 -0700

    Add files via upload

---
+++ sketch_nov16a.ino
@@ -0,0 +1,32 @@
+/*
+ * schematic layout
+ *  a b
+ *  c d
+ *  e f
+ */
+
+#define a 2
+#define b 3
+//con tinue
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(a, OUTPUT);
+  //continue
+
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void letA(){
+  //letter a
+  digitalWrite(a, HIGH);
+  digitalWrite(b, LOW);
+  //continue
+
+  dela
+}
